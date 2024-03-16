4398dbb2db5ca224a0c00a754afd55ba08cb7602
Hoàng Dương <hoangvanduong1359@gmail.com>
2024-03-17 01:45:47 +0700

    create API google feature and redesign ELS-View , new form google_translate

--- src/main/java/org/example/els/ELSApplication.java
+++ src/main/java/org/example/els/ELSApplication.java
@@ -1,10 +1,11 @@
 package org.example.els;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-//import javafx.scene.web.WebView;
 
 import java.io.IOException;
 
@@ -12,12 +13,11 @@ public class ELSApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ELSApplication.class.getResource("ELS-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 718, 478);
+        Scene scene = new Scene(fxmlLoader.load(), 850, 600);
         stage.setTitle("ELSApplication");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/org/example/els/ELSController.java
+++ src/main/java/org/example/els/ELSController.java
@@ -1,14 +1,34 @@
 package org.example.els;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ELSController {
-//    @FXML
-//    private Label welcomeText;
+    protected static Stage stage;
+    protected static Scene scene;
+    protected static FXMLLoader fxmlLoader;
+    @FXML
+    private Button dictionary_menu;
+    @FXML
+    private Button google_translate_menu;
+    @FXML
+    private Button game_menu;
+    @FXML
+    private AnchorPane pane;
+    @FXML
+    protected WebView definitionView;
+    @FXML
+    public void openFormGoogle(ActionEvent event) throws IOException {
+        SceneManage.showScene(stage,scene,fxmlLoader,event,"google_translate.fxml");
+    }
 
-//    @FXML
-//    protected void onHelloButtonClick() {
-//        welcomeText.setText("Welcome to JavaFX Application!");
-//    }
 }
