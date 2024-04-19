3a707b200dcc0968b958ea89ce91efbb97990ec5
Hoàng Dương <hoangvanduong1359@gmail.com>
2024-04-20 01:53:20 +0700

    edit feature deleteWord and editWord

--- src/main/java/org/example/els/editWordController.java
+++ src/main/java/org/example/els/editWordController.java
@@ -1,5 +1,7 @@
 package org.example.els;
 
+import dictionary.DatabaseConnection;
+import dictionary.DictionaryManagementDatabase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,6 +9,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class editWordController extends baseFormController{
@@ -20,13 +24,14 @@ public class editWordController extends baseFormController{
         super.initialize();
         commit.setVisible(false);
     }
-    public void findWord(ActionEvent event) {
+    public void findWord(ActionEvent event) throws SQLException {
         String word = e_word.getText();
         if (word==null){
             newAlert(stage,"Find word","","Vui lòng nhập từ");
             return;
         }
-        String meaning= dictionaryManagement.Search(word);
+        //String meaning= dictionaryManagement.Search(word);
+        String meaning= DictionaryManagementDatabase.Search(word,true);
         if(meaning.equals("NO FOUND")){
             newAlert(stage,"Find word","","Không tìm thấy từ");
         }
@@ -37,13 +42,23 @@ public class editWordController extends baseFormController{
     }
     public void editWord(ActionEvent event){
         try {
-            dictionaryManagement.setExplain(e_word.getText(),v_word.getHtmlText());
-            bookwriter.append("# "+e_word.getText()+" "+v_word.getHtmlText()+"\n");
+            //bookwriter.append("- "+s+"\n");
+            Connection conn = DatabaseConnection.connect("jdbc:sqlite:src\\Data\\database.db");
+            String sql = "INSERT INTO bookmark (user_id,target,explain,type) VALUES (?,?,?,?)";
+            PreparedStatement pr = conn.prepareStatement(sql);
+            pr.setInt(1,user.getId());
+            pr.setString(2,e_word.getText());
+            pr.setString(3,v_word.getHtmlText());
+            pr.setString(4,"#");
+            pr.executeUpdate();
+            conn.close();
+            //dictionaryManagement.setExplain(e_word.getText(),v_word.getHtmlText());
+            //bookwriter.append("# "+e_word.getText()+" "+v_word.getHtmlText()+"\n");
             newAlert(stage,"Edit word","","Đã sửa thành công");
             e_word.setText("");
             v_word.setHtmlText("");
-        } catch (IOException e) {
-            System.out.println("IOException in editword: " + e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
