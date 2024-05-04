ec9cb757a0b713d15d67630b85429df00319253f
Hoàng Dương <hoangvanduong1359@gmail.com>
2024-05-05 00:58:30 +0700

    hiệu chỉnh lại cá nhân hóa cho việc sửa dụng từ điển
    sửa phần edit add delete theo từng loại từ điển, thêm cột mode vào bảng bookmark recentlist

--- src/main/java/Bookmark/bookmarkmanagement.java
+++ src/main/java/Bookmark/bookmarkmanagement.java
@@ -15,14 +15,15 @@ import static org.example.els.SceneManage.newAlert;
 import static org.example.els.baseFormController.getmode;
 
 public class bookmarkmanagement {
-    public static boolean targetExists(Connection connection, String targetValue, User user) throws SQLException {
+    public static boolean targetExists(Connection connection, String targetValue, User user, String MODE) throws SQLException {
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
         try {
-            String selectQuery = "SELECT COUNT(*) FROM bookmark WHERE target = ? AND user_id = ?";
+            String selectQuery = "SELECT COUNT(*) FROM bookmark WHERE target = ? AND user_id = ? AND mode= ?";
             preparedStatement = connection.prepareStatement(selectQuery);
             preparedStatement.setString(1, targetValue);
             preparedStatement.setInt(2, user.getId());
+            preparedStatement.setString(3, MODE);
             resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
                 int count = resultSet.getInt(1);
@@ -38,9 +39,9 @@ public class bookmarkmanagement {
         return false;
     }
 
-    public static void addWord(String target, String explain, User user,boolean av) {
+    public static void addWord(String target, String explain, User user, String MODE) {
         try {
-            String se = DictionaryManagementDatabase.Search(target, av);
+            String se = DictionaryManagementDatabase.Search(target, MODE);
             if (!se.equals("NO FOUND") && !se.equals("you have deleted this word")) {
                 //return 0;
                 newAlert("AddWord", "", "Từ đã tồn tại");
@@ -48,12 +49,13 @@ public class bookmarkmanagement {
                 //dictionaryManagement.insert(e, v);
                 //bookwriter.append("+ "+e+" "+v+"\n");
                 Connection connection = DatabaseConnection.connect("jdbc:sqlite:src\\Data\\database.db");
-                String insertQuery = "INSERT INTO bookmark (target, type, explain,user_id) VALUES (?, ?, ?,?)";
+                String insertQuery = "INSERT INTO bookmark (target, type, explain,user_id,mode) VALUES (?, ?, ?, ?, ?)";
                 PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
                 insertStatement.setString(1, target);
                 insertStatement.setString(2, "+");
                 insertStatement.setString(3, explain);
                 insertStatement.setInt(4, user.getId());
+                insertStatement.setString(5, MODE);
                 insertStatement.executeUpdate();
                 newAlert("AddWord", "", "Thêm từ thành công");
             } else {
@@ -64,31 +66,33 @@ public class bookmarkmanagement {
         }
     }
 
-    public static void editWord(String target, String explain, User user,boolean av) {
+    public static void editWord(String target, String explain, User user, String MODE) {
         try {
-            String se = DictionaryManagementDatabase.Search(target, av);
+            String se = DictionaryManagementDatabase.Search(target, MODE);
             if (!target.isEmpty() && !explain.isEmpty()) {
                 //dictionaryManagement.insert(e, v);
                 //bookwriter.append("+ "+e+" "+v+"\n");
                 Connection connection = DatabaseConnection.connect("jdbc:sqlite:src\\Data\\database.db");
-                if (bookmarkmanagement.targetExists(connection, target, user)) {
-                    String updateQuery = "UPDATE bookmark SET type = ?, explain = ? WHERE target = ? AND user_id = ?";
+                if (bookmarkmanagement.targetExists(connection, target, user, MODE)) {
+                    String updateQuery = "UPDATE bookmark SET type = ?, explain = ? WHERE target = ? AND user_id = ? AND mode = ?";
                     PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
                     updateStatement.setString(1, "#");
                     updateStatement.setString(2, explain);
                     updateStatement.setString(3, target);
                     updateStatement.setInt(4, user.getId());
+                    updateStatement.setString(5, MODE);
                     updateStatement.executeUpdate();
-                    newAlert("Edit word","","Đã sửa thành công");
+                    newAlert("Edit word", "", "Đã sửa thành công");
                 } else {
-                    String insertQuery = "INSERT INTO bookmark (target, type, explain,user_id) VALUES (?, ?, ?,?)";
+                    String insertQuery = "INSERT INTO bookmark (target, type, explain,user_id,mode) VALUES (?, ?, ?,?,?)";
                     PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
                     insertStatement.setString(1, target);
                     insertStatement.setString(2, "#");
                     insertStatement.setString(3, explain);
                     insertStatement.setInt(4, user.getId());
+                    insertStatement.setString(5, MODE);
                     insertStatement.executeUpdate();
-                    newAlert("Edit word","","Đã sửa thành công");
+                    newAlert("Edit word", "", "Đã sửa thành công");
                 }
             } else {
                 newAlert("editWord", "", "Vui lòng nhập đủ thông tin từ");
@@ -97,27 +101,40 @@ public class bookmarkmanagement {
             throw new RuntimeException(e);
         }
     }
-    public static void deleteWord(String word , boolean av) {
+
+    public static void deleteWord(String word, String MODE) {
         try {
-            if(word==null){
-                newAlert("Delete Word","","Vui lòng nhập từ");
+            if (word == null) {
+                newAlert("Delete Word", "", "Vui lòng nhập từ");
             }
             //else if(dictionaryManagement.Search(s).equals("NO FOUND")){
-            else if(DictionaryManagementDatabase.Search(word,av).equals("NO FOUND")){
-                newAlert("Delete Word","","Không tìm thấy từ");
-            }
-            else {
-                //dictionaryManagement.remove(s);
-                newAlert("Delete Word","","Xóa từ thành công");
-                //bookwriter.append("- "+s+"\n");
+            else if (DictionaryManagementDatabase.Search(word, MODE).equals("NO FOUND")) {
+                newAlert("Delete Word", "", "Không tìm thấy từ");
+            } else {
                 Connection conn = DatabaseConnection.connect("jdbc:sqlite:src\\Data\\database.db");
-                String sql = "INSERT INTO bookmark (user_id,target,type) VALUES (?,?,?)";
-                PreparedStatement pr = conn.prepareStatement(sql);
-                pr.setInt(1, baseFormController.user.getId());
-                pr.setString(2,word);
-                pr.setString(3,"-");
-                pr.executeUpdate();
-                conn.close();
+                if (bookmarkmanagement.targetExists(conn, word, baseFormController.user, MODE)) {
+                    String updateQuery = "UPDATE bookmark SET type = ? WHERE target = ? AND user_id = ? AND mode = ?";
+                    PreparedStatement updateStatement = conn.prepareStatement(updateQuery);
+                    updateStatement.setString(1, "-");
+                    updateStatement.setString(2, word);
+                    updateStatement.setInt(3, baseFormController.user.getId());
+                    updateStatement.setString(4, MODE);
+                    updateStatement.executeUpdate();
+                    newAlert("Delete word", "", "Xóa từ thành công");
+                } else {
+                    //dictionaryManagement.remove(s);
+                    newAlert("Delete Word", "", "Xóa từ thành công");
+                    //bookwriter.append("- "+s+"\n");
+                    String sql = "INSERT INTO bookmark (user_id,target,type, mode) VALUES (?,?,?,?)";
+                    PreparedStatement pr = conn.prepareStatement(sql);
+                    pr.setInt(1, baseFormController.user.getId());
+                    pr.setString(2, word);
+                    pr.setString(3, "-");
+                    pr.setString(4, MODE);
+                    pr.executeUpdate();
+                    conn.close();
+                }
+
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- src/main/java/dictionary/DictionaryManagementDatabase.java
+++ src/main/java/dictionary/DictionaryManagementDatabase.java
@@ -13,26 +13,22 @@ public class DictionaryManagementDatabase {
     public static final String dict_hh_URL = "jdbc:sqlite:src\\Data\\dict_hh.db";
     public static final String DATABASE_URL = "jdbc:sqlite:src\\Data\\database.db";
 
-    public static ObservableList prexSearch(String text, boolean av) throws SQLException {
-        String NAME_TABLE = null;
-        if (av) {
-            NAME_TABLE = new String("av");
-        } else {
-            NAME_TABLE = new String("va");
-        }
+    public static ObservableList prexSearch(String text, String MODE) throws SQLException {
+        String NAME_TABLE = MODE;
         List<String> res = new ArrayList<String>();
         Connection conn2 = DatabaseConnection.connect(DATABASE_URL);
         String sql = new String();
-        sql = "SELECT type , target FROM bookmark WHERE target LIKE ? AND type = '+' ORDER BY (CASE  WHEN target = ? THEN 1 ELSE 0 END ) DESC ";
+        sql = "SELECT type , target FROM bookmark WHERE target LIKE ? AND type = '+' AND mode = ? ORDER BY (CASE  WHEN target = ? THEN 1 ELSE 0 END ) DESC ";
         PreparedStatement pr = conn2.prepareStatement(sql);
         pr.setString(1,"%" + text + "%");
-        pr.setString(2,text);
+        pr.setString(2,MODE);
+        pr.setString(3,text);
         ResultSet rs1 = pr.executeQuery(); // lấy kết quả
         while (rs1.next()) {
             res.add(rs1.getString("target"));
         }
         conn2.close();
-        sql = "SELECT word FROM " + NAME_TABLE + " WHERE word LIKE ? ORDER BY (CASE  WHEN word = ? THEN 1 ELSE 0 END ) DESC ";
+        sql = "SELECT word FROM " + NAME_TABLE + " WHERE  word LIKE ? ORDER BY (CASE  WHEN word = ? THEN 1 ELSE 0 END ) DESC ";
         //SELECT * FROM av WHERE text LIKE ? ORDER BY (CASE WHEN text = ? THEN 1 ELSE 0 END) DESC"
 
         Connection conn = DatabaseConnection.connect(dict_hh_URL);
@@ -48,24 +44,20 @@ public class DictionaryManagementDatabase {
         return result;
     }
 
-    public static String Search(String text, boolean av) throws SQLException {
+    public static String Search(String text, String MODE) throws SQLException {
 
-        String NAME_TABLE = null;
-        if (av) {
-            NAME_TABLE = new String("av");
-        } else {
-            NAME_TABLE = new String("va");
-        }
+        String NAME_TABLE = MODE;
         // nếu mà bookmark có từ này -> lấy type
         // -> if "-" then no found
         // -> if "+" or "#" then return explain
         // else bookmark không có -> search av
         String sql = new String();
         Connection conn1 = DatabaseConnection.connect(DATABASE_URL);
-        sql = "SELECT type, target , explain FROM bookmark WHERE user_id = ? AND target =?";
+        sql = "SELECT type, target , explain FROM bookmark WHERE user_id = ? AND target =? AND mode = ?";
         PreparedStatement pr = conn1.prepareStatement(sql);
         pr.setInt(1, baseFormController.user.getId());
         pr.setString(2, text);
+        pr.setString(3, MODE);
         ResultSet resultSet = pr.executeQuery();
         if (resultSet.next()) {
             String type = resultSet.getString("type");
